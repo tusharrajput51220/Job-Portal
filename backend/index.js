f3db88238bf3709345883591fc2b8577bee73056
tusharrajput51220 <tusharrajput51220@gmail.com>
2024-09-01 08:11:59 +0530

    hello

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: "https://job-portal-omega-five.vercel.app",
+  // origin: process.env.CLIENT_URL || "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use("/api/v1", router);
+
+app.get("*", (req,res)=>{
+  res.json("Hello")
+})
 
 const port = process.env.PORT || 8000;
 app.listen(port, async () => {
