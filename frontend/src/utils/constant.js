522af5f2b8c146add7bbca504653b67d074b5d92
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-09-02 09:16:02 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const BASEURL="https://job-portal-backend-p7dz.onrender.com/"
+export const BASEURL="https://job-portal-backend-p7dz.onrender.com/api/v1/"
 // export const BASEURL="http://localhost:8000/api/v1/"
